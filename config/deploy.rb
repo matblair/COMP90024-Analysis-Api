3783f1d4f0557dc6431c3b02e618fdaac3e867c4
Mathew Blair <mathew.blair@unimelb.edu.au>
2015-05-12 16:34:19 +1000

    Uploading Analysis API to it's own repo for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+set :application, 'analysis_api'
+set :repo_url, 'git@github.com:matblair/COMP90024-Analysis-Api.git'
+
+set :deploy_to, '/home/ubuntu/analysis_api'
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+set :stage, :production
+server '144.6.227.63', user: 'ubuntu', roles: %w{web app}
