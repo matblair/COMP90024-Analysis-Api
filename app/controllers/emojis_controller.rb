c30f0325e5608992a4a5187372c7b54d2b9b3f69
Edward Crupi <e.crupi@student.unimelb.edu.au>
2015-05-22 18:38:06 +1000

    Completed emoji/general almost finished emoji/:emoji_code/locations

--- app/controllers/emojis_controller.rb
+++ app/controllers/emojis_controller.rb
@@ -1,16 +1,30 @@
-class HashtagsController < ApplicationController
+class EmojisController < ApplicationController
   include EmojiRepresentor
   include DemographicParser
 
+  # Find 
+  before_action :validate_emoji_code, only: [:location]
   def top_ten 
-  	# Make couch request for all emoji 
-  	response = Couchdb.make_request 'tweets', 'emojis', 'general'
-  	# Sort it
-  	
+  	# Make couch request for all emoji
+  	response = (Couchdb.make_request 'tweets', 'emojis', 'general', {'group'=>true, 'group_level'=>1})['rows']
+    # Sort it and return  hash
+    response = Emojis.top_ten response
+    render json: response
   end
 
   def location
+    # Make couch request for all emoji
+    response = (Couchdb.make_request 'tweets', 'emojis', 'general_byloc', {'group'=>true, 'group_level'=>9})['rows']
+    # Return the selected emoji's location based on :emoji_code param
+    response = Emojis.find_all response, @emoji['emoji_code'].to_s
+    render json: response
+  end
 
+  private
+  def validate_emoji_code
+    if params.has_key?('emoji_code')
+      @emoji = {"emoji_code" => params[:emoji_code]}
+    end
   end
 
 end
--- app/models/emojis.rb
+++ app/models/emojis.rb
@@ -1,3 +1,12 @@
-class Emojis
+class Emojis < ActiveRecord::Base
 
+	def self.top_ten emoji_array
+		return Hash[(emoji_array.sort_by {|hash| hash['value']['count']}).reverse.first(10).map {|key, value| [key, value]}]
+	end
+
+	def self.find_all emoji_array, emoji
+		#emoji_array = emoji_array.select{|key| key[2] == emoji }
+		emoji_array = emoji_array.select{|key, value| key[2] == emoji }
+		return (emoji_array)
+	end
 end
